6c98db89d1cd5363ce2027cfe69f967bbc0b8d44
The Omar <faroque.ae@gmail.com>
2019-09-20 14:04:47 -0500

    making submission ready

--- leetcode/contests/contest_153/DistanceBetweenBustops.py
+++ leetcode/contests/contest_153/DistanceBetweenBustops.py
@@ -1,6 +1,28 @@
 import unittest
 
 
+def clockwise_distance(distance, start, destination):
+    dist = 0
+    for counter in range(start, destination):
+        dist += distance[counter]
+    return dist
+
+
+def anti_clockwise_distance(distance, start, destination):
+    if start == destination:
+        return 0
+    dist = 0
+    # zero to point
+    for counter in range(0, start):
+        dist += distance[counter]
+    # point to end
+    for counter in range(destination, len(distance) - 1):
+        dist += distance[counter]
+    # end to point
+    dist += distance[len(distance) - 1]
+    return dist
+
+
 class TestDistanceBetweenBusStop(unittest.TestCase):
 
     def distance_between_bus_stops(self, distance, start, destination):
@@ -10,14 +32,26 @@ class TestDistanceBetweenBusStop(unittest.TestCase):
         :type destination: int
         :rtype: int
         """
-        return 100
+        if start < destination:
+            clock_distance = clockwise_distance(distance, start, destination)
+        else:
+            clock_distance = clockwise_distance(distance, destination, start)
+
+        # clock_distance = clockwise_distance(distance, start, destination)
+        print('clockwise distance' + str(clock_distance))
+
+        if start < destination:
+            anti_clock_distance = anti_clockwise_distance(distance, start, destination)
+        else:
+            anti_clock_distance = anti_clockwise_distance(distance, destination, start)
+        return min(clock_distance, anti_clock_distance)
 
     def test_first_distance_between_bus_stops(self):
-        distance = [1, 2, 3, 4]
-        start = 0
-        destination = 1
+        distance = [7, 10, 1, 12, 11, 14, 5, 0]
+        start = 7
+        destination = 2
         result = self.distance_between_bus_stops(distance, start, destination)
-        print(result)
+        self.assertEqual(result, 17, "distance should be 4")
 
 
 if __name__ == '__main__':
